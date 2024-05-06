7a4d3dc5e52c3d3c644642713b306dfcdd11e8bf
Miranda Honkanen <miranda.honkanen@gmail.com>
2024-05-06 15:53:09 +0300

    12.17

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -8,5 +8,10 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './testSetup.js', 
+  },
+  server: {
+    watch: {
+      usePolling: true
+    }
   }
 })
